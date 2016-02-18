c737a97144bbaad69ff68d92885cedafc594ad2e
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-02-18 07:53:58 +0200

    Base structure

--- rank/collect/source/__init__.py
+++ rank/collect/source/__init__.py
@@ -1,2 +1,10 @@
+from collections import namedtuple
+Result = namedtuple("Result", ["text"])
+
+
 class Base(object):
-    pass
+    def auth(self, user, pwd) -> bool:
+        pass
+
+    def search(self, key) -> list:
+        pass
