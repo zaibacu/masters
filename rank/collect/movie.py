737722557263284cbdd482231d579ebb855eb350
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-03-20 18:00:36 +0200

    Include torrent.lt comments

--- rank/collect/movie.py
+++ rank/collect/movie.py
@@ -1,6 +1,12 @@
 def get_comments(title: str) -> list:
     from rank.collect.source.lm import LMSource
+    from rank.collect.source.torrent_lt import TorrentLtSource
     import os
     lm = LMSource()
     lm.auth(os.getenv("lm_user"), os.getenv("lm_pass"))
-    return [comment for comment in lm.search(title)]
+    torrent_lt = TorrentLtSource()
+    torrent_lt.auth(os.getenv("torrentlt_user"), os.getenv("torrentlt_pass"))
+
+    lm_comments = [comment for comment in lm.search(title)]
+    torrentlt_comments = [comment for comment in torrent_lt.search(title)]
+    return lm_comments + torrentlt_comments
