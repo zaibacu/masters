aa030e6b9dc725ff413472ef5d63934f038d0e7b
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-03-20 19:29:54 +0200

    Don't fail if any of engines fails

--- rank/collect/movie.py
+++ rank/collect/movie.py
@@ -1,15 +1,33 @@
-def get_comments(title: str) -> list:
+def get_comments(title: str, data: dict = None) -> list:
     from rank.collect.source.lm import LMSource
     from rank.collect.source.torrent_lt import TorrentLtSource
     from rank.collect.source.filmai_in import FilmaiInSource
     import os
     lm = LMSource()
-    lm.auth(os.getenv("lm_user"), os.getenv("lm_pass"))
+
+    def get_param(key):
+        if data and key in data:
+            return data[key]
+        else:
+            return os.getenv(key)
+
+    lm.auth(get_param("lm_user"), get_param("lm_pass"))
     torrent_lt = TorrentLtSource()
-    torrent_lt.auth(os.getenv("torrentlt_user"), os.getenv("torrentlt_pass"))
+    torrent_lt.auth(get_param("torrentlt_user"), get_param("torrentlt_pass"))
     filmai_in = FilmaiInSource()
 
-    lm_comments = [comment for comment in lm.search(title)]
-    torrent_lt_comments = [comment for comment in torrent_lt.search(title)]
-    filmai_in_comments = [comment for comment in filmai_in.search(title)]
+    try:
+        lm_comments = [comment for comment in lm.search(title)]
+    except:
+        lm_comments = []
+
+    try:
+        torrent_lt_comments = [comment for comment in torrent_lt.search(title)]
+    except:
+        torrent_lt_comments = []
+
+    try:
+        filmai_in_comments = [comment for comment in filmai_in.search(title)]
+    except:
+        filmai_in_comments = []
     return lm_comments + torrent_lt_comments + filmai_in_comments
