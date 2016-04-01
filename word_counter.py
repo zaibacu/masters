94db03f7e131087b460055dd37db0eb8af631e64
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-01 07:29:05 +0300

    Take L most common items. Don't display count

--- word_counter.py
+++ word_counter.py
@@ -1,12 +1,15 @@
 from collections import Counter
 
 
-def main(_in, _out):
+def main(args, _in, _out):
     words = Counter(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), _in.read().split("\n"))))
-    for word, count in words.items():
-        _out.write("{0}\t{1}\n".format(word, count))
+    for word, count in words.most_common(args.l):
+        _out.write("{0}\n".format(word))
 
 
 if __name__ == "__main__":
     import sys
-    main(sys.stdin, sys.stdout)
+    from argparse import ArgumentParser()
+    parser = ArgumentParser()
+    parser.add_argument("-l", help="Take l most common items", type=int, default=1000)
+    main(parser.parse_args(), sys.stdin, sys.stdout)
