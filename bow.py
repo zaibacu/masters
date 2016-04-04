a26b358ea489e911e30fc8ef8de359b3988afc1c
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-04 07:39:54 +0300

    Ability to create bow out of input and dictionary

--- bow.py
+++ bow.py
@@ -11,3 +11,26 @@ def vw_model(rating, bow):
     # [Label] [Importance] [Base] [Tag]|Namespace Features |Namespace Features
     return "{0} 1.0 |bow {1}".format(rating, bow_string(bow))
 
+
+def clean(x):
+    return x.replace("\n", "").strip()
+
+
+def main(args, _in, _out):
+    _dict = []
+    with open(args.f, "r") as f:
+        for l in f:
+            _dict.append(set(map(clean, l.split(","))))
+
+    raw = list(map(clean, _in.readlines()))
+
+    bow = compute_bow(raw, _dict)
+    _out.write("{0}".format(bow_string(bow)))
+
+
+if __name__ == "__main__":
+    import sys
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("-f", help="Dictionary file")
+    main(parser.parse_args(), sys.stdin, sys.stdout)
