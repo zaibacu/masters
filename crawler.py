d2ca2df8467627b18b05b1db8879ca2d8f7c9277
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-06 20:51:38 +0300

    Small tweaks

--- crawler.py
+++ crawler.py
@@ -5,8 +5,8 @@ def purge(raw):
 def main(_in, _out):
     title = _in.read()
     from rank.collect.movie import get_comments
-    for comment in map(purge, get_comments(title)):
-        _out.write("{0}\n".format(comment))
+    for comment in get_comments(title.strip()):
+        _out.write("{0}\n".format(purge(comment)))
 
 if __name__ == "__main__":
     import sys
