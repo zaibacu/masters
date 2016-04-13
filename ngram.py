5e4f3aba4e3f50451576d8f10c1c1863620f27b0
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-13 20:52:47 +0300

    Use n-gram util

--- ngram.py
+++ ngram.py
@@ -1,11 +1,10 @@
-def ngram(lst: list, n: int):
-    return zip(*[lst[i:] for i in range(n)])
+from rank.util.ngram import make_ngram
 
 
 def main(args, _in, _out):
     items = _in.read().split("\n")
-    for item in ngram(items, args.n):
-        _out.write("({0})\n".format(item))
+    for item in make_ngram(items, args.n):
+        _out.write("({0})\n".format("|".join(item)))
 
 if __name__ == "__main__":
     import sys
