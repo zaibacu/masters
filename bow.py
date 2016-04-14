0cfb1a28847f4433a6b6931c90d17d51042a1a9f
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-14 19:31:14 +0300

    Improved BOW algorithm

--- bow.py
+++ bow.py
@@ -3,12 +3,17 @@ from rank.util.ngram import unpack
 
 
 def compute_bow(words: list, dictionary: list, matcher: callable, limit: int):
-    return [any([True
-                 for s in d
-                 for w in words
-                 if matcher(s, w) <= limit
-                 ])
+    def word_inside_dict(_dict: list, word: str) -> bool:
+        return any([True
+                    for s in _dict
+                    if matcher(s, word) <= limit
+                    ])
 
+    def match_dict_item(item: tuple, word: tuple) -> bool:
+        return all([word_inside_dict(i, w)
+                    for i, w in zip(item, word)])
+
+    return [any([match_dict_item(d, w) for w in words])
             for d in dictionary]
 
 
@@ -31,11 +36,11 @@ def clean(x):
 
 def load_dict(f):
     for l in f:
-        yield set(map(unpack, map(clean, l.split(","))))
+        yield set(map(lambda x: tuple(map(clean, x)), map(lambda x: set(x.split(",")), unpack(l))))
 
 
 def load_raw(f):
-    return map(unpack, map(clean, f.readlines()))
+    return map(lambda x: tuple(map(clean, x)), map(unpack, f.readlines()))
 
 
 class BowTestCase(unittest.TestCase):
@@ -67,6 +72,31 @@ class BowTestCase(unittest.TestCase):
         result = compute_bow(_raw, _dict, levenshtein, 0)
         expected = [True, False]
         self.assertEqual(expected, result)
+
+    def test_ngram_bow(self):
+        from io import StringIO
+        from rank.util import levenshtein
+        from argparse import Namespace
+        d = StringIO("hello,hi\nworld\n")
+        r = StringIO("hi\nworld\n")
+
+        """
+        (hi,hello|world) vs (hi|world)
+        """
+        d_buff = StringIO()
+        r_buff = StringIO()
+        import ngram
+        ngram.main(Namespace(n=2), d, d_buff)
+        ngram.main(Namespace(n=2), r, r_buff)
+        d_buff.seek(0)
+        r_buff.seek(0)
+
+        _dict = list(load_dict(d_buff))
+        _raw = list(load_raw(r_buff))
+        print("Dict: {0}".format(_dict))
+        print("Text: {0}".format(_raw))
+        result = compute_bow(_raw, _dict, levenshtein, 0)
+        print(result)
 
 
 def main(args, _in, _out):
