a00dd9d5c637df08b225182d8e3696fb5b829200
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-14 08:09:11 +0300

    Make levenshtein work with tuples

--- rank/util/__init__.py
+++ rank/util/__init__.py
@@ -1,5 +1,8 @@
 from Levenshtein import distance
 
 
-def levenshtein(w1, w2):
-    return distance(w1, w2)
+def levenshtein(w1: tuple, w2: tuple) -> int:
+    return min([
+        distance(d1, d2)
+        for d1, d2 in zip(w1, w2)
+    ])
