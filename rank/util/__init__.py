a0dca95dece506bff43d3a0d22a9abbc759959fd
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-14 19:03:39 +0300

    back to using simple strings

--- rank/util/__init__.py
+++ rank/util/__init__.py
@@ -3,9 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def levenshtein(w1: tuple, w2: tuple) -> int:
+def levenshtein(w1: str, w2: str) -> int:
     logger.debug("Distance between {0} and {1}".format(w1, w2))
-    return min([
-        distance(d1, d2)
-        for d1, d2 in zip(w1, w2)
-    ])
+    return distance(w1, w2)
