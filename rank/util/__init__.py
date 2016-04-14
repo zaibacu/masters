54705f19f20b779d97f08e7ecb6144c6232f911c
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-14 08:14:45 +0300

    A bit of logging

--- rank/util/__init__.py
+++ rank/util/__init__.py
@@ -1,7 +1,10 @@
 from Levenshtein import distance
+import logging
+logger = logging.getLogger(__name__)
 
 
 def levenshtein(w1: tuple, w2: tuple) -> int:
+    logger.debug("Distance between {0} and {1}".format(w1, w2))
     return min([
         distance(d1, d2)
         for d1, d2 in zip(w1, w2)
