381e6f34ca69b932796d00c3dade1fc90e694be7
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-15 07:28:12 +0300

    Apply unknown labels to crawler comments as well

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ def main(_in, _out):
     title = _in.read()
     from rank.collect.movie import get_comments
     for comment in get_comments(title.strip()):
-        _out.write("{0}\n".format(purge(comment)))
+        _out.write("?\t{0}\n".format(purge(comment)))
 
 if __name__ == "__main__":
     import sys
