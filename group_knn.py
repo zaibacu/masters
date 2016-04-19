c7843b4356c90c59cca917f445111e0556b1c390
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-19 10:01:56 +0300

    Result should be set of tuples

--- group_knn.py
+++ group_knn.py
@@ -7,9 +7,13 @@ logger = logging.getLogger(__name__)
 
 
 def distance(g1, g2, matcher):
+    error_dist = 100
     logger.debug("Got {0} and {1} to match".format(g1, g2))
+    if isinstance(g1, float) or isinstance(g2, float):
+        return error_dist
+
     if len(g1) == 0 or len(g2) == 0:
-        return 100  # big enough number
+        return error_dist  # big enough number
 
     return min([
                 matcher(i1, i2)
@@ -27,36 +31,44 @@ def clustering(groups, matcher, max_dist):
                       for g2 in groups
                       if g1 != g2], columns=["left", "right"])
 
-    df = df.where(np.tril(np.ones(df.shape)).astype(np.bool)).dropna()
+    df = df.where(np.tril(np.ones(df.shape)).astype(np.bool))
+
+    logger.debug("Working on df: {0}".format(df))
 
     def df_dist(left, right):
         return list([distance(pair[0], pair[1], matcher) for pair in zip(left, right)])
 
     while True:
         df["distance"] = df_dist(df["left"], df["right"])
+        logger.debug(df)
         _min = df.ix[df["distance"].idxmin()]
         if _min["distance"] < max_dist:
             for i in _min["right"]:
                 _min["left"].add(i)
             _min["right"].clear()
-            logger.debug(df)
         else:
             break
 
-    return df["left"]
+    return set(filter(lambda x: len(x) > 0, map(lambda x: tuple(sorted(x)), df["left"])))
 
 
 class KNNTestCase(unittest.TestCase):
-    @staticmethod
-    def setUpClass():
+    def setUp(self):
         logging.basicConfig(level=logging.DEBUG)
 
     def test_grouping_step(self):
         from rank.util import levenshtein
         groups = [{"a", "b", "c"}, {"c", "d", "f"}]
-        result = clustering(groups, levenshtein, 2).values.tolist()
-        expected = [{"a", "b", "c", "d", "f"}]
-        self.assertEqual(expected, result)
+        result = clustering(groups, levenshtein, 2)
+        expected = {("a", "b", "c", "d", "f")}
+        self.assertSetEqual(expected, result)
+
+    def test_different_sets(self):
+        from rank.util import levenshtein
+        groups = [{"labas", "vakaras"}, {"rytoj", "bus"}]
+        result = clustering(groups, levenshtein, 2)
+        expected = {("labas", "vakaras"), ("bus", "rytoj")}
+        self.assertSetEqual(expected, result)
 
 
 def main(args, _in, _out):
