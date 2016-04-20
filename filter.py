0af96b3f4b460688e36adb3704497e5d9367ace6
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-20 13:28:27 +0300

    Filter by word length and ignore list

--- filter.py
+++ filter.py
@@ -1,3 +1,19 @@
+def main(args, _in, _out):
+    ignore_list = []
+    if args.i:
+        with open(args.i, "r") as f:
+            ignore_list = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), f.read().split("\n"))))
+
+    words = filter(lambda x: len(x) > 0, map(lambda x: x.strip(), _in.read().split("\n")))
+    for word in words:
+        if len(word) > args.l and word not in ignore_list:
+            _out.write("{0}\n".format(word))
+
 if __name__ == "__main__":
+    from argparse import ArgumentParser
     import sys
-    sys.stdout.write(sys.stdin.read())
+
+    parser = ArgumentParser()
+    parser.add_argument("-l", help="Minimal word length", default=3)
+    parser.add_argument("-i", help="Ignore list")
+    main(parser.parse_args(), sys.stdin, sys.stdout)
