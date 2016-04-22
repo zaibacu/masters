14bee5f6998d0ad28008b5aad6a991d3b1f81561
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-22 12:51:21 +0300

    Feature parser

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,22 @@
+def parse_bow(line):
+    import re
+    feature_pattern = re.compile(r"((?P<feature_id>f\d+):(?P<feature_value>-?\d[.]\d+))")
+    label_part, bow_part = line.split("|", 1)
+    label, _ = label_part.split(" ", 1)
+    features = [(f.group("feature_id"), f.group("feature_value"))
+                for f in feature_pattern.finditer(bow_part)
+                ]
+
+    return label, features
+
+
 def main(args, _in, _out):
-    pass
+    # format: -1 1.0 |bow f0:1.0 f1:1.0 f2:1.0 f3:1.0 f4:1.0 f5:1.0
+    data = [parse_bow(line)
+            for line in map(lambda x: x.strip(), _in.read().split("\n"))
+            if len(line) > 0
+            ]
+
 
 if __name__ == "__main__":
     import sys
