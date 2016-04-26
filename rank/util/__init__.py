15f001ba85ebcb6242e29e309c7295f8a5088bb8
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-26 09:36:54 +0300

    other comparison options

--- rank/util/__init__.py
+++ rank/util/__init__.py
@@ -7,3 +7,17 @@ def levenshtein(w1: str, w2: str) -> int:
     result = distance(w1, w2)
     logger.debug("Distance between {0} and {1} is {2}".format(w1, w2, result))
     return result
+
+
+def stem(w1: str, w2: str) -> int:
+    from stemmer import stemmer
+    st = stemmer.Stemmer()
+    return chars(st.stem(w1), st.stem(w2))
+
+
+def chars(w1: str, w2: str) -> int:
+    from itertools import zip_longest
+    return sum([1
+                for c1, c2 in zip_longest(w1, w2)
+                if c1 != c2
+                ])
