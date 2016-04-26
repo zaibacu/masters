fed9a1b1901745a2041b5fbaa20e879473415f28
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-26 18:23:00 +0300

    some refactoring

--- word_counter.py
+++ word_counter.py
@@ -1,12 +1,16 @@
 from collections import Counter
 
 
-def main(args, _in, _out):
-    words = Counter(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), _in.read().split("\n"))))
-    if args.all:
-        gen = map(lambda x: (x[1], x[0]), enumerate(words))
+def counter(words: list, limit: int, all: bool) -> list:
+    cnt = Counter(words)
+    if all:
+        return map(lambda x: (x[1], x[0]), enumerate(cnt))
     else:
-        gen = words.most_common(args.l)
+        return cnt.most_common(limit)
+
+
+def main(args, _in, _out):
+    gen = counter(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), _in.read().split("\n"))), args.l, args.all)
 
     for word, count in gen:
         _out.write("{0}\n".format(word))
