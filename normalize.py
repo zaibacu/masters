dfbfdab95e0e7b61b743dd0201cf0c4f565e5e1f
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-04-27 09:15:07 +0300

    Allow to handle accents

--- normalize.py
+++ normalize.py
@@ -7,6 +7,22 @@ def parse_line(line: str) -> tuple:
     return label, get_words(text)
 
 
+def purge_accents(word: str) -> str:
+    from functools import reduce
+    mapping = [
+        ("ą", "a"),
+        ("č", "c"),
+        ("ę", "e"),
+        ("ė", "e"),
+        ("į", "i"),
+        ("š", "s"),
+        ("ų", "u"),
+        ("ū", "u"),
+        ("ž", "z")
+    ]
+    return reduce(lambda s, translation: s.replace(*translation), mapping, word)
+
+
 def main(args, _in, _out):
     st = stemmer.Stemmer()
     data = [parse_line(line)
@@ -14,12 +30,23 @@ def main(args, _in, _out):
             if len(line) > 0
             ]
 
+    def pipe(word: str) -> str:
+        if args.stem:
+            word = st.stem(word)
+
+        if args.accents:
+            word = purge_accents(word)
+
+        return word
+
     for label, words in data:
-        _out.write("{0} | {1}\n".format(label, " ".join([st.stem(w) for w in words])))
+        _out.write("{0} | {1}\n".format(label, " ".join([pipe(w) for w in words])))
 
 
 if __name__ == "__main__":
     import sys
     from argparse import ArgumentParser
     parser = ArgumentParser()
+    parser.add_argument("--stem", action="store_true", help="Use stemmer?")
+    parser.add_argument("--accents", action="store_true", help="Remove accents?")
     main(parser.parse_args(), sys.stdin, sys.stdout)
