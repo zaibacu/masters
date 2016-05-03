f06e198b2cf0694a8e18f3306777fcc41f17f845
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-05-03 07:21:16 +0300

    Some tweaks to work better with the system

--- crawler.py
+++ crawler.py
@@ -1,12 +1,11 @@
-def purge(raw):
-    return raw.replace("\n", " ")
+from rank.util import purge
 
 
 def main(_in, _out):
     title = _in.read()
     from rank.collect.movie import get_comments
     for comment in get_comments(title.strip()):
-        _out.write("?\t{0}\n".format(purge(comment)))
+        _out.write("| {0}\n".format(purge(comment)))
 
 if __name__ == "__main__":
     import sys
